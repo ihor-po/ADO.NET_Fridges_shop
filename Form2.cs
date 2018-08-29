343ee6ccaadabfbf4a58404afcd0091bd5e0511c
Ihor Porotikov <sobikashi@gmail.com>
2018-08-29 15:19:35 +0300

    get fridge info

--- Form2.cs
+++ Form2.cs
@@ -111,6 +111,11 @@ namespace FridgeShop
             }
         }
 
+        /// <summary>
+        /// Change selected fridge
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Salling_cb_fridges_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (salling_cb_fridges.SelectedItem.ToString() != "")
@@ -118,11 +123,13 @@ namespace FridgeShop
                 selling_frigo_quantity.Enabled = true;
                 selling_frigo_quantity.Text = "0";
                 selling_frigo_quantity.Focus();
+                GetFridgoInfo(salling_cb_fridges.SelectedItem.ToString());
             }
             else
             {
                 selling_frigo_quantity.Enabled = false;
                 selling_frigo_quantity.Text = "";
+                selling_frigo_info.Text = "";
             }
             //ShowMessage(salling_cb_fridges.SelectedItem.ToString());
         }
@@ -147,10 +154,8 @@ namespace FridgeShop
 
                 foreach (DataRow row in rows)
                 {
-                    salling_cb_fridges.Items.Add(row[0].ToString() + "   " + row[1].ToString());
+                    salling_cb_fridges.Items.Add($"{row[0].ToString()}   {row[1].ToString()}");
                 }
-
-          
             }
             catch (Exception ex)
             {
@@ -168,5 +173,38 @@ namespace FridgeShop
         {
             MessageBox.Show(msg, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
+
+        private void GetFridgoInfo(string markModel)
+        {
+            int index = markModel.IndexOf("   ");
+            int strLength = markModel.Length;
+            string mark = markModel.Substring(0, index);
+            string model = markModel.Substring(index + 3, strLength - (index + 3));
+
+            string sql = $"SELECT * FROM fridges WHERE mark = '{mark}' AND model = '{model}';" ;
+
+            try
+            {
+                ds = new DataSet();
+
+                conn.Open();
+
+                adapter = new SqlDataAdapter(sql, conn);
+                cmd = new SqlCommandBuilder(adapter);
+                adapter.Fill(ds, "fre");
+                DataRowCollection fRows = ds.Tables["fre"].Rows;
+
+                selling_frigo_info.Text = $"\t\t {fRows[0][0]}   {fRows[0][1]}   {fRows[0][2]}   {fRows[0][3]}    {fRows[0][4]}\n";
+
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+
+            conn?.Close();
+        }
+
+
     }
 }
